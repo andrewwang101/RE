92a1ed8739d8d6cfb9889092c40916e07511d7b7
Andrew <andrewwang101@gmail.com>
2019-10-19 18:45:16 +0800

    版本回朔
    之前AVI誤存造成檔案過大

---
+++ RE/Assets/MainFunc.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainFunc : MonoBehaviour
+{
+    public GameObject PlayButton;
+    public GameObject PrefButton;
+    public GameObject QuitButton;
+
+    //public GameObject LanguageButton;
+    //public GameObject VolUpButton;
+    //public GameObject VolDownButton;
+    public GameObject BackButton;
+
+    public GameObject Pref_Page;
+
+    public static MainFunc Instance;
+    public int language = 0;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        Instance = this;
+        //LanguageButton.SetActive(false);
+        //VolUpButton.SetActive(false);
+        //VolDownButton.SetActive(false);
+        BackButton.SetActive(false);
+
+        Pref_Page.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void StartGame()
+    {
+
+    }
+
+    public void Preference()
+    {
+        PlayButton.SetActive(false);
+        PrefButton.SetActive(false);
+        QuitButton.SetActive(false);
+
+        //LanguageButton.SetActive(true);
+        //VolUpButton.SetActive(true);
+        //VolDownButton.SetActive(true);
+        BackButton.SetActive(true);
+
+        Pref_Page.SetActive(true);
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+
+    public void Language_Toggle()
+    {
+
+    }
+
+    public void VolumnUP()
+    {
+
+    }
+
+    public void VolumnDown()
+    {
+
+    }
+
+    public void BacktoMenu()
+    {
+        //LanguageButton.SetActive(false);
+        //VolUpButton.SetActive(false);
+        //VolDownButton.SetActive(false);
+        BackButton.SetActive(false);
+
+        PlayButton.SetActive(true);
+        PrefButton.SetActive(true);
+        QuitButton.SetActive(true);
+
+        Pref_Page.SetActive(false);
+    }
+
+}
