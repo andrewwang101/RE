ba864fbd4cb48529bf7bfec9ac47177cf061687c
Andrew <andrewwang101@gmail.com>
2019-10-20 05:14:06 +0800

    Temp

--- RE/Assets/TTCRoom.cs
+++ RE/Assets/TTCRoom.cs
@@ -9,6 +9,9 @@ public class TTCRoom : MonoBehaviour
     public GameObject StoryBtn;
     public GameObject[] CharOS = new GameObject[3];
     public int TTCindex = 0;
+    public GameObject TTCBtn;
+    public GameObject CloseTTCBtn;
+    public GameObject[] TTCResult = new GameObject[2];
 
     // Start is called before the first frame update
     void Start()
@@ -18,12 +21,34 @@ public class TTCRoom : MonoBehaviour
         {
             CharOS[i].SetActive(false);
         }
+
+        TTCBtn.SetActive(false);
+        CloseTTCBtn.SetActive(false);
+        for (int i = 0;i < 2;i++)
+        {
+            TTCResult[i].SetActive(false);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void OpenTTCPanel()
+    {
+        TTCResult[TTCindex].SetActive(true);
+        if (TTCindex == 0) CloseTTCBtn.SetActive(true);
+        else StoryBtn.SetActive(true);
+        TTCBtn.SetActive(false);
+    }
+
+    public void CloseTTCPanel()
+    {
+        TTCResult[TTCindex].SetActive(false);
+        StoryBtn.SetActive(true);
+        TTCindex = 1;
     }
 
     public void CmdMainStory()
@@ -37,16 +62,18 @@ public class TTCRoom : MonoBehaviour
 
             case 7:
                 CharOS[0].SetActive(false);
-                //StoryBtn.SetActive(false);//去和物品互動
+                StoryBtn.SetActive(false);//去和物品互動
+                TTCBtn.SetActive(true);
                 break;
-
+            
             case 8://這顆衛星看起來有GNSS喔
                 CharOS[1].SetActive(true);
                 break;
 
             case 9:
                 CharOS[1].SetActive(false);
-                //StoryBtn.SetActive(false);//去和物品互動
+                StoryBtn.SetActive(false);//去和物品互動
+                TTCBtn.SetActive(true);
                 break;
 
             case 10://喔喔???
